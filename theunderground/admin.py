eae25fc025b1fde26cf75ad2f2719558d823055e
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-10-21 16:34:16 -0400

    Update admin.py

--- theunderground/admin.py
+++ theunderground/admin.py
@@ -49,8 +49,17 @@ else:
 if enabled:
     @app.route('/theunderground/signin')
     def signin():
+      if current_user.is_authenticated:
+          return redirect(url_for('index'))
       form = LoginForm()
-      return render_template('login.html',form=form)
+      if form.validate_on_submit():
+          user = User.query.filter_by(username=form.username.data).first()
+          if user is None or not user.check_password(form.password.data):
+              flash('Invalid username or password')
+              return redirect(url_for('login'))
+          login_user(user, remember=False)
+          return redirect(url_for('index'))
+      return render_template('login.html', form=form)
         
     @login_required
     @app.route('/theunderground/admin')
