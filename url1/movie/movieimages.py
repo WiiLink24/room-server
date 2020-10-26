416c248d1149d1ce7432ece7440b2ae11457e261
JG <zurgeg@gmail.com>
2020-10-26 10:23:36 -0400

    Fix security bugs after testing related update + import it :)

--- url1/movie/movieimages.py
+++ url1/movie/movieimages.py
@@ -1,6 +1,20 @@
-#  Handles movie images
+# Handles movie images
 from room import app
-@app.route('/url1/movie/<movid>/<imgid>.img')
-def handle_movieimage(movid,imgid):
-    return serve_from_directory(movid,imgid+'.img')
+from flask import send_from_directory
+print('movies')
+@app.route('/url1/testing')
+def testing():
+    return 'hmm'
+@app.route('/url1/movie/c8/<imgid>')
+def handle_movieimage(imgid):
+    # Solution to the fact that flask does not like 2 <> in a row
+    return send_from_directory('static',imgid)
+
+@app.route('/url1/movie/a8/<imgid>')
+def handle_movieimage_a8(imgid): 
+    # Solution to the fact that flask does not like 2 <> in a row
+    return send_from_directory('static',imgid)
     
+
+
+
--- url1/special/page.py
+++ url1/special/page.py
@@ -1,11 +1,10 @@
 # Handles pages and logo images
 from room import app
-@app.route('/url1/special/<page>/img/<img>.img')
+from flask import send_from_directory
+@app.route('/url1/special/<page>/img/<img>')
 def handleimg(page,img):
     # Handles logo images, for instance:
     # GET /url1/special/1/img/g1234.img
-    # Gets pageimgs/1_g1234.img
-    return send_from_directory('pageimgs',page+'_'+img+'.img')
-@app.route('/url1/special/<page>/page.xml')
-def handlepage(page):
-    # Implement with the webconsole and Mii Parade
+    # Gets g1234.img
+    print(img)
+    return send_from_directory('static',img)
