ef27edf36619f70c5837ece040abbe08e3743701
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-10-26 23:38:55 -0400

    Update movieimages.py

--- url1/movie/movieimages.py
+++ url1/movie/movieimages.py
@@ -14,11 +14,5 @@ def handle_movieimage(imgid):
 def handle_movieimage_a8(imgid): 
     # Solution to the fact that flask does not like 2 <> in a row
     return send_from_directory('static',imgid)
-@app.route('/url1/movie/c4/<imgid>')
-def handle_movieimage_c4(imgid): 
-    # Solution to the fact that flask does not like 2 <> in a row
-    return send_from_directory('static',imgid)
-    
-
 
 
