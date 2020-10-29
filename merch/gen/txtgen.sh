6546ac17d774ca274efe08c00525029e2cbf7bf0
6100m <31662463+6100m@users.noreply.github.com>
2020-10-29 16:11:08 -0600

    Create txtgen.sh

---
+++ merch/gen/txtgen.sh
@@ -0,0 +1,15 @@
+echo Enter absolute path of config.json 
+echo eg: $PWD
+echo Enter it here: 
+read varmain
+echo Enter rb data
+echo Unless you need to intentionally change it, it should be rb
+echo Enter it here:
+read varsecn
+echo Enter what you named your primary storage file:
+read varmisc
+echo Enter what you named your secondary storage file:
+read varothr
+echo $varmain > $PWD/$varmisc
+echo $varsecn > $PWD/$varothr
+echo Done.
