404ded5c3008551b348246bebceff16109ebda0a
6100m <31662463+6100m@users.noreply.github.com>
2020-10-29 12:56:27 -0600

    Update add_to_tables.sh

--- merge/gen/add_to_tables.sh
+++ merge/gen/add_to_tables.sh
@@ -2,4 +2,8 @@ echo Enter the unique ID you placed into create.sh:
 read varmain
 echo Where is the python file located on this server that loads it?:
 read varsecn
-echo \n$varmain > $varsecn/tables.txt
+echo What do you want the primary storage file to be named?:
+read varothr
+echo What do you want the secondary storage file to be named?:
+read varrest
+echo \n$varmain\n$varothr\n$varrest > $varsecn/tables.txt
