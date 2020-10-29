8800b82ca73f5685ef4837a58292d71c36b4bb85
6100m <31662463+6100m@users.noreply.github.com>
2020-10-29 16:27:04 -0600

    Create password.py

---
+++ merch/gen/password.py
@@ -0,0 +1,10 @@
+from random import *
+from time import *
+hash = random.getrandbits(128)
+print("password: %032x" % hash)
+print("Exiting in 3...")
+time.sleep(1)
+print("Exiting in 2...")
+time.sleep(1)
+print("Exiting in 1...")
+time.sleep(1)
