6f2093570de105bc458cacd449007e30e8335460
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-02 20:48:19 -0500

    Update shop.py

--- shopurl/shop.py
+++ shopurl/shop.py
@@ -1,4 +1,5 @@
-from room import app
+from room import app, db
+from models import TestShopppingBag
 from flask import send_from_directory
 @app.route('/shopurl/index.esf')
 def shop():
@@ -10,14 +11,31 @@ def test_order():
 def test_register()
   data = request.get_headers()
   if data['X-Wii-Number'] in ['disallowed wii numbers/already registered wii numbers']:
-    return {'status':1,message:'Wii Number already registered'}
+    return {'status':1,message:'Wii Number already registered or access denied'}
   else:
+    t = TestShoppingBag(wii_number=data['X-Wii-Number'],points=500) # Everyone starts out with 500 points in testing
+    db.session.add(t) # Add our shopping bag to the session
+    db.session.commit() # Flush the session
     return {'status':0,message:'registered'}
 @app.route('/v1/testing/addpoints',methods=['POST'])
 def test_add_points():
-  print('Implement database column first')
+  # Find the user with the Wii Number
+  data = request.get_headers()
+  bag = TestShoppingBag.query.filter_by(wii_number=data['X-Wii-Number'])
+  # Add to the points value
+  bag.points += data['X-Points-Num']
+  # Add and commit
+  db.session.add(bag)
+  db.session.commit()
 @app.route('/v1/testing/removepoints',methods=['POST'])
 def test_remove_points():
-  print('Implement database column first')
+  # Find the user with the Wii Number
+  data = request.get_headers()
+  bag = TestShoppingBag.query.filter_by(wii_number=data['X-Wii-Number'])
+  # Subtract from the points value
+  bag.points -= data['X-Points-Num']
+  # Add and commit
+  db.session.add(bag)
+  db.session.commit()
 
   
