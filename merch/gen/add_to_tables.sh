f2373ae80094f96705a96f81d2db4cd993df3ce4
6100m <31662463+6100m@users.noreply.github.com>
2020-11-03 16:49:11 -0700

    Port this to Py

--- merch/gen/add_to_tables.sh
+++ merch/gen/add_to_tables.sh
@@ -1,11 +1,12 @@
-echo Current Path: $PWD
-echo Enter the unique ID you want:
-read varmain
-echo Where is the python file located on this server that loads it?:
-read varsecn
-echo What do you want the primary storage file to be named?:
-read varothr
-echo What do you want the secondary storage file to be named?:
-read varrest
-echo \n$varmain\n$varothr\n$varrest > $varsecn/tables.txt
-echo $varmain > $varsecn/id.txt
+import os
+os.system('echo Current Path: $PWD')
+data1 = raw_input('Enter Unique ID that you want : ')
+data2 = raw_input('What do you want the primary storage file to be named : ')
+data3 = raw_input('What do you want the secondary storage file to be named? : ')
+data4 = "/n"
+outF1 = open("tables.txt", "w")
+textList1 = [data4, data1, data2, data3]
+outF1.close()
+outF2 = open("id.txt", "w")
+textList2 = [data1]
+outF2.close()
