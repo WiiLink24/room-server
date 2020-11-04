31117234dab519923d1189f5e56904f234abb878
6100m <31662463+6100m@users.noreply.github.com>
2020-11-03 17:40:28 -0700

    Update and rename configreader.sh to configreader.py

---
+++ merch/gen/configreader.py
@@ -0,0 +1,5 @@
+argdata1 = sys.argv[1] #Line Number
+argdata2 = sys.argv[2] #File
+file = open(argdata2)
+all_lines = file.readlines()
+print(all_lines[argdata1])
--- merch/gen/configreader.sh
+++
@@ -1,9 +0,0 @@
-while getopts a:b: flag
-do
-    case "${flag}" in
-        a) argmain=${OPTARG};;
-        b) argsecn=${OPTARG};;
-    esac
-done
-variable=$(awk $argmain $argsecn)
-echo $variable
