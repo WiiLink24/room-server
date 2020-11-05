ca390b5bc338626524bff71487b0c3e8ae986a82
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-05 18:31:26 -0500

    Make encodemii no longer require os.system

--- theunderground/encodemii.py
+++ theunderground/encodemii.py
@@ -1,24 +1,13 @@
-# Encodes images for use in the parade.
-# TODO: Use a library such as Pillow instead of external calls
-
-import os
-
-
-def encode(input_file, output):
-    """
-    encode(input,output) -> None
-    Arguments:
-    input (str): file to encode
-    output (str): where to encode it to
-    """
-    os.system("cat {} | base64 > {}".format(input, output))
-
-
-def resize(input_file, output):
-    # DO NOT SET OUTPUT TO .IMG
-    # IT WILL **FAIL** AS IT CAN
-    # NOT DETERMINE WHAT TO OUTPUT AS!!
-    # INSTEAD USE SHUTIL.MOVE!
-    os.system(
-        "magick convert {} -resize 160x120 -interlace none {}".format(input, output)
-    )
+# Encodes images 
+from PIL import Image
+import base64
+from os import remove
+def parade_encode(in):
+    size = 184,80
+    im = Image.open(infile)
+    im = im.resize(size)
+    im.save("temp.jpg", "JPEG")
+    encodemii = open("temp.jpg","wb")
+    data = base64.b64encode(encodemii.read())
+    encodemii.close()
+    remove("temp.jpg")
