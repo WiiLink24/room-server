9ea703527cabf6142d7da0af74b4449b2b3359e0
6100m <31662463+6100m@users.noreply.github.com>
2020-11-05 10:51:08 -0700

    Remove unneeded functions.

--- merch/gen/utilsbylarsen.py
+++ merch/gen/utilsbylarsen.py
@@ -38,12 +38,6 @@ def log(msg, level):  # TODO: Use number levels, strings are annoying
         elif level == "CRITICAL":
             logger.critical(msg)
 
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except:
-        pass
-
 
 """Pack integers to specific type."""
 
@@ -69,34 +63,6 @@ def u32(data):
         data = 0
     return struct.pack(">I", data)
 
-
-def u32_littleendian(data):
-    if not 0 <= data <= 4294967295:
-        log("u32 little endian out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack("<I", data)
-
-# Signed integer
-
-def s8(data):
-    if not -128 <= data <= 127:
-        log("s8 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">b", data)
-
-
-def s16(data):
-    if not -32768 <= data <= 32767:
-        log("s16 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">h", data)
-
-
-def s32(data):
-    if not -2147483648 <= data <= 2147483647:
-        log("s32 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">i", data)
 
 def pad(amnt):
     buffer = ""
