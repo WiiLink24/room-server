3bbb104d73e60712804d80dafa11e294241e6f27
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-06 11:15:45 -0500

    Update search.py

--- url1/search.py
+++ url1/search.py
@@ -1,7 +1,9 @@
 from elasticsearch import Elasticsearch
 from flask import request
+from helpers import xml_node_name
 es = Elasticsearch('http://localhost:9200')
 @app.route('/url2/search.cgi')
+@xml_node_name('SearchMovies')
 def search():
     q = request.args.get('q')
     es.search(index='tv_index', body={'query': {'match': {'title': q}}})
