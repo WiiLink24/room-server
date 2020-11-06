cdaf903d6f8f3a96ca05a1e6d0ea60303401c39d
6100m <31662463+6100m@users.noreply.github.com>
2020-11-05 18:55:36 -0700

    v2, added attachement support

--- merch/gen/digital-content-sender.py
+++ merch/gen/digital-content-sender.py
@@ -1,14 +1,27 @@
-import sendgrid
 import os
-from sendgrid.helpers.mail import *
-def send(to, from, subjectdata, contentdata, key):
-  sg = sendgrid.SendGridAPIClient(api_key=os.environ.get(key))
-  from_email = Email(from)
-  to_email = To(to)
-  subject = subjectdata
-  content = Content("text/plain", contentdata)
-  mail = Mail(from_email, to_email, subject, content)
-  response = sg.client.mail.send.post(request_body=mail.get())
-  print(response.status_code)
-  print(response.body)
-  print(response.headers)
+import base64
+import sys
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+def send(to, fromdata, subjectdata, nameofattachment, key, filetype):
+  message = Mail(
+      from_email=fromdata,
+      to_emails=to,
+      subject='Thank you for your purchase! Here is the DLC you purchased!',
+      html_content='<strong>Check attachments, we attached your purchase!</strong><br><br><br><strong>Sent by 6100m\'s DLC Mail Bot</strong>'
+  )
+  with open(nameofattachment, 'rb') as f:
+      data = f.read()
+      f.close()
+  encoded_file = base64.b64encode(data).decode()
+  attachedFile = Attachment(
+      FileContent(encoded_file),
+      FileName(nameofattachment),
+      FileType(filetype),
+      Disposition('attachment')
+  )
+  message.attachment = attachedFile
+  sg = SendGridAPIClient(os.environ.get(key))
+  response = sg.send(message)
+  print(response.status_code, response.body, response.headers)
+  sys.exit(0)
