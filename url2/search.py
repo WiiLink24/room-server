59d8c986a7783a9bb27e948c2ca4372a1f6ae33d
Spotlight <spotlight@joscomputing.space>
2020-11-07 19:56:13 -0600

    Use date and time for search

--- url2/search.py
+++ url2/search.py
@@ -1,7 +1,7 @@
 from config import elasticsearch_url
 from elasticsearch import Elasticsearch
 from flask import request
-from helpers import xml_node_name, RepeatedElement, current_date
+from helpers import xml_node_name, RepeatedElement, current_date_and_time
 from room import app
 
 es = Elasticsearch(elasticsearch_url)
@@ -35,8 +35,8 @@ def search():
                         "movieid": i,
                         "title": show_ids[i],
                         "genre": 1,
-                        "strdt": current_date(),
-                        "pop": 0
+                        "strdt": current_date_and_time(),
+                        "pop": 0,
                     }
                 )
             )
