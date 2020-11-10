b3f710cc432842c8c062b6517c6495c67aee00fb
6100m <31662463+6100m@users.noreply.github.com>
2020-11-10 14:12:24 -0700

    v1.0.6, even more minifications.

--- merch/gen/order-photos.py
+++ merch/gen/order-photos.py
@@ -8,23 +8,28 @@ from datadog import (
   api,
 )
 import utilsbylarsen as i
-z = 'rb' #Generic Text Writing Opcode
-def orderpwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
-  if g["execute_all"]:
+def pwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
+  ab="production"
+  ac="send_logs"
+  ad="use_pickle"
+  ae="execute_all"
+  z='rb'
+  if g[ae]:
     with open("./config.json", "rb") as f:
     g = a.load(f)
-  if g["production"] and g["send_logs"] and g["load_options_file"] and g["use_pickle"]:
+  if g[ab] and g[ac] and g["load_options_file"] and g[ad]:
     h = open(options.dat, z)
     options = b.load(h)
-  if g["production"] and g["send_logs"] and g["use_pickle"]:
+  if g[ab] and g[ac] and g[ad]:
     i.setup_log(g["sentry_url"], False)
-  if g["production"] and g["use_private_key"] and g["execute_all"]:
+  if g[ab] and g["use_private_key"] and g[ae]:
     j = b.load(open(password.dat, z)) 
     #j is the Private key, still need to import this fully, it doesn't work yet.
     y = g["pwinty_cdn_url"] + "/" + x
     #Set x to the name of the image you want printed on the photo
-    if g["production"]
-      order = d.Order.create(
+    if g[ab]
+      aa=d.Order
+      order = aa.create(
           recipient_name =         	k,
           address_1 =              	l,
           address_2 =              	m,
@@ -35,7 +40,7 @@ def orderpwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
           country_code =           	r,
           qualityLevel =           	s
       )
-      photo = order.photos.create(
+      photo = aa.photos.create(
         type =   	t,
         url =    	y,
         md5Hash =	u,
