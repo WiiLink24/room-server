3b3b4ab07312367099f2b54af2cab4fe1e9df388
6100m <31662463+6100m@users.noreply.github.com>
2020-11-10 10:24:03 -0700

    v1.0.5, yet even more simplications.

--- merch/gen/order-photos.py
+++ merch/gen/order-photos.py
@@ -7,9 +7,7 @@ from datadog import (
   initialize,
   api,
 )
-from utilsbylarsen import (
-  setup_log,
-)
+import utilsbylarsen as i
 z = 'rb' #Generic Text Writing Opcode
 def orderpwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
   if g["execute_all"]:
@@ -19,47 +17,33 @@ def orderpwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
     h = open(options.dat, z)
     options = b.load(h)
   if g["production"] and g["send_logs"] and g["use_pickle"]:
-    setup_log(g["sentry_url"], False)
+    i.setup_log(g["sentry_url"], False)
   if g["production"] and g["use_private_key"] and g["execute_all"]:
-    i = open(password.dat, 'rb')
-    j = b.load(i) #Private key, still need to import this fully, it doesn't work yet.
-    y = g["pwinty_cdn_url"] + "/" + x #Set x to the name of the image you want printed on the photo
-    k = recipient_name1
-    l = address1
-    m = address2
-    n = address_town_or_city1
-    o = state_or_county1
-    p = postal_or_zip_code1
-    q = destination_country_code1
-    r = countrycode
-    s = qualityLevel1
-    t = type1
-    u = md5Hash1
-    v = copies1
-    w = sizing1
+    j = b.load(open(password.dat, z)) 
+    #j is the Private key, still need to import this fully, it doesn't work yet.
+    y = g["pwinty_cdn_url"] + "/" + x
+    #Set x to the name of the image you want printed on the photo
     if g["production"]
       order = d.Order.create(
-          recipient_name =         	recipient_name1,
-          address_1 =              	address1,
-          address_2 =              	address2,
-          address_town_or_city =   	address_town_or_city1,
-          state_or_county =        	state_or_county1,
-          postal_or_zip_code =     	postal_or_zip_code1,
-          destination_country_code =	destination_country_code1,
-          country_code =           	countrycode,
-          qualityLevel =           	qualityLevel1
+          recipient_name =         	k,
+          address_1 =              	l,
+          address_2 =              	m,
+          address_town_or_city =   	n,
+          state_or_county =        	o,
+          postal_or_zip_code =     	p,
+          destination_country_code =	q,
+          country_code =           	r,
+          qualityLevel =           	s
       )
       photo = order.photos.create(
-        type =   	type1,
+        type =   	t,
         url =    	y,
-        md5Hash =	md5Hash1,
-        copies = 	copies1,
-        sizing = 	sizing1
+        md5Hash =	u,
+        copies = 	v,
+        sizing = 	w
       )
       initialize(**options)
-      t = e.localtime()
-      timevar = e.strftime("%H:%M:%S", t)
-      title = "Latest Order Script Status @ " + timevar
+      title = "Latest Order Script Status @ " + e.strftime("%H:%M:%S", e.localtime())
       text = 'Order Script for Photos was ran!'
       tags = ['version:1', 'application:python']
       api.Event.create(title=title, text=text, tags=tags)
