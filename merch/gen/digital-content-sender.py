8b598462dda0124007ef6d4770325fa4002d6480
6100m <31662463+6100m@users.noreply.github.com>
2020-11-10 10:43:31 -0700

    v2.3.1.2, simplifications/minifcations, and quality of life improvements.

--- merch/gen/digital-content-sender.py
+++ merch/gen/digital-content-sender.py
@@ -1,38 +1,46 @@
-import os
-import base64
-import sys
-import utilsbygloom
+import os as a
+import base64 as b
+import sys as c
+import utilsbygloom as d
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
-def send(to, fromdata, subjectdata, nameofattachment, key, filetype, offset):
-  if offset == 0:
-    message = Mail(
-        from_email=fromdata,
-        to_emails=to,
-        subject='Thank you for your purchase! Here is the DLC you purchased!',
-        html_content='<strong>Check attachments, we attached your purchase!</strong><br><br><br><strong>Sent by 6100m\'s DLC Mail Bot</strong>'
+def send(l, m, n, o, p, r):
+  if r == 0:
+    s = 'rb' #Generic Text File Writing Opcode
+    t = 'attachment' #Attachment Idenitifer Data
+    u = SendGridAPIClient #APIClient Class Definition
+    v = a.environ.get(o) #Key Data Definition
+    w = sg.send #Sendgrid Sender Class Definition
+    x = b.b64encode #Base64 Encode Class Definition
+    y = 'Thank you for your purchase! Here is the DLC you purchased!' #Hardcoded Subject Data
+    z = '<strong>Check attachments, we attached your purchase!</strong><br><br><br><strong>Sent by 6100m\'s DLC Mail Bot</strong>' #Hardcoded HTML Data
+    k = Mail(
+        from_email=m,
+        to_emails=l,
+        subject=y,
+        html_content=z
     )
-    with open(nameofattachment, 'rb') as f:
-        data = f.read()
+    with open(n, s) as f:
+        e = f.read()
         f.close()
-    encoded_file = base64.b64encode(data).decode()
-    attachedFile = Attachment(
-        FileContent(encoded_file),
-        FileName(nameofattachment),
-        FileType(filetype),
-        Disposition('attachment')
+    q = x(e).decode()
+    j = Attachment(
+        FileContent(q),
+        FileName(n),
+        FileType(p),
+        Disposition(t)
     )
-    message.attachment = attachedFile
-    sg = SendGridAPIClient(os.environ.get(key))
-    response = sg.send(message)
+    message.attachment = j
+    sg = u(v)
+    response = w(k)
     print(response.status_code, response.body, response.headers)
-    sys.exit(0)
-  elif offset == 1:
-    offset2 = 9
-    data = "Congrats, you accessed the secret feature!"
-    echodata = utilsbygloom.returnnumber(offset2)
-    utilsbygloom.spotlightutil(echodata, data)
+    c.exit(0)
+  elif r == 1:
+    g = 9
+    h = "Congrats, you accessed the secret feature!"
+    echodata = utilsbygloom.returnnumber(g)
+    d.spotlightutil(echodata, h)
   else:
-    returndata = 0
-    utilsbygloom.miscutil(returndata)
-    return returndata
+    i = 0
+    d.miscutil(i)
+    return i
