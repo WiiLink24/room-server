cd7e3e56df295f07803f2fe47fa21da2c354ecd7
6100m <31662463+6100m@users.noreply.github.com>
2020-11-10 12:37:59 -0700

    v2, minify it heavily.

--- merch/gen/utilsbylarsen.py
+++ merch/gen/utilsbylarsen.py
@@ -1,33 +1,25 @@
-import errno
-import logging
-import os
-import requests
-import sentry_sdk
-import struct
+import logging as f
+import requests as g
+import sentry_sdk as e
+import struct as a
 from sentry_sdk.integrations.logging import LoggingIntegration
-
-"""Unification of utilities used by all scripts."""
-
-requests.packages.urllib3.disable_warnings()  # This is so we don't get some warning about SSL.
-
+g.packages.urllib3.disable_warnings()  # This is so we don't get some warning about SSL.
 production = False
 p_errors = False
-
 def setup_log(sentry_url, print_errors):
     global logger, production
     sentry_logging = LoggingIntegration(
-        level=logging.INFO,
-        event_level=logging.INFO
+        level=f.INFO,
+        event_level=f.INFO
     )
-    sentry_sdk.init(dsn=sentry_url, integrations=[sentry_logging])
+    e.init(dsn=sentry_url, integrations=[sentry_logging])
     logger = logging.getLogger(__name__)
     p_errors = print_errors
     production = True
-
-def log(msg, level):  # TODO: Use number levels, strings are annoying
+def log(msg, level):  
+    # TODO: Use number levels, strings are annoying
     if p_errors:
         print(msg)
-
     if production:
         if level == "VERBOSE":
             logger.debug(msg)
@@ -37,34 +29,22 @@ def log(msg, level):  # TODO: Use number levels, strings are annoying
             logger.warning(msg)
         elif level == "CRITICAL":
             logger.critical(msg)
-
-
-"""Pack integers to specific type."""
-
-# Unsigned integers
-
-def u8(data):
-    if not 0 <= data <= 255:
-        log("u8 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">B", data)
-
-
-def u16(data):
-    if not 0 <= data <= 65535:
-        log("u16 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">H", data)
-
-
-def u32(data):
-    if not 0 <= data <= 4294967295:
-        log("u32 out of range: %s" % data, "INFO")
-        data = 0
-    return struct.pack(">I", data)
-
-
-def pad(amnt):
-    buffer = ""
-    for _ in range(amnt): buffer += "\0"
-    return buffer
+def u8(b):
+    if not 0 <= b <= 255:
+        log("u8 out of range: %s" % b, "INFO")
+        b = 0
+    return a.pack(">B", b)
+def u16(b):
+    if not 0 <= b <= 65535:
+        log("u16 out of range: %s" % b, "INFO")
+        b = 0
+    return a.pack(">H", b)
+def u32(b):
+    if not 0 <= b <= 4294967295:
+        log("u32 out of range: %s" % b, "INFO")
+        b = 0
+    return a.pack(">I", b)
+def pad(c):
+    d = ""
+    for _ in range(c): d += "\0"
+    return d
