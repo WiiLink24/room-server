0748862cca08707e6510092c7bf8fd1d0a79f8f4
6100m <31662463+6100m@users.noreply.github.com>
2020-11-11 13:24:52 -0700

    v2, nearly perfect other than dat encryption, config imports, and dynamic config imports. (Which those three things still need to be done, mind you)

--- merch/gen/orderphotos.py
+++ merch/gen/orderphotos.py
@@ -2,12 +2,11 @@ import json as a
 import pickle as b
 import os as c
 import pwinty as d
-import time as e
-from datadog import (
-  initialize,
-  api,
-)
+import random as ag
+import sys as al
+import datadog as am
 import utilsnotbyme as i
+import ntplib as ar
 def pwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
   ab="production"
   ac="send_logs"
@@ -18,8 +17,11 @@ def pwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
     with open("./config.json", z) as f:
     g = a.load(f)
   if g[ab] and g[ac] and g["load_options_file"] and g[ad]:
-    h = open(options.dat, z)
-    options = b.load(h)
+    if c.path.exists("options.dat"):
+      h = open(options.dat, z)
+      options = b.load(h)
+    else:
+      i.log("options.dat does not exist!! %s" % b, "CRITICAL")
   if g[ab] and g[ac] and g[ad]:
     i.setup_log(g["sentry_url"], False)
   if g[ab] and g["use_private_key"] and g[ae]:
@@ -28,8 +30,8 @@ def pwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
     y = g["pwinty_cdn_url"] + "/" + x
     #Set x to the name of the image you want printed on the photo
     if g[ab]
-      aa=d.Order
-      order = aa.create(
+      af=d.Order
+      order = af.create(
           recipient_name=k,
           address_1=l,
           address_2=m,
@@ -40,16 +42,70 @@ def pwinty(k, l, m, n, o, p, q, r, s, t, u, v, w, x):
           country_code=r,
           qualityLevel=s
       )
-      photo = aa.photos.create(
+      photo = af.photos.create(
         type=t,
         url=y,
         md5Hash=u,
         copies=v,
         sizing=w
       )
-      initialize(**options)
-      ab = "Latest Order Script Status @ " + e.strftime("%H:%M:%S", e.localtime())
-      ac = 'Order Script for Photos was ran!'
-      ad = ['version:1', 'application:python']
-      api.Event.create(title=ab, text=ac, tags=ad)
-      c._exit(0)
+      am.initialize(**options)
+      c = ar.NTPClient()
+      randomizer = ag.randint(0, 24)
+      if randomizer==1:
+        response = c.request('time1.google.com', version=3)
+      elif randomizer==2:
+        response = c.request('time2.google.com', version=3)
+      elif randomizer==3:
+        response = c.request('time3.google.com', version=3)
+      elif randomizer==4:
+        response = c.request('time4.google.com', version=3)
+      elif randomizer==5:
+        response = c.request('0.pool.ntp.org', version=3)
+      elif randomizer==6:
+        response = c.request('1.pool.ntp.org', version=3)
+      elif randomizer==7:
+        response = c.request('2.pool.ntp.org', version=3)
+      elif randomizer==8:
+        response = c.request('time-a-g.nist.gov', version=3)
+      elif randomizer==9:
+        response = c.request('time-c-g.nist.gov', version=3)
+      elif randomizer==10:
+        response = c.request('time-d-g.nist.gov', version=3)
+      elif randomizer==11:
+        response = c.request('time-e-g.nist.gov', version=3)
+      elif randomizer==12:
+        response = c.request('time-a-wwv.nist.gov', version=3)
+      elif randomizer==13:
+        response = c.request('time-b-wwv.nist.gov', version=3)
+      elif randomizer==14:
+        response = c.request('time-c-wwv.nist.gov', version=3)
+      elif randomizer==15:
+        response = c.request('time-d-wwv.nist.gov', version=3)
+      elif randomizer==16:
+        response = c.request('time-e-wwv.nist.gov', version=3)
+      elif randomizer==17:
+        response = c.request('time-a-b.nist.gov', version=3)
+      elif randomizer==18:
+        response = c.request('time-b-b.nist.gov', version=3)
+      elif randomizer==19:
+        response = c.request('time-c-b.nist.gov', version=3)
+      elif randomizer==20:
+        response = c.request('time-d-b.nist.gov', version=3)
+      elif randomizer==21:
+        response = c.request('time-e-b.nist.gov', version=3)
+      elif randomizer==22:
+        response = c.request('time.nist.gov', version=3)
+      elif randomizer==23:
+        response = c.request('utcnist.colorado.edu', version=3)
+      elif randomizer==24:
+        response = c.request('tcnist2.colorado.edu', version=3)
+      else:
+        response = c.request('time.cloudflare.com', version=3)
+      response.offset
+      ah = str(ak.fromtimestamp(response.v, timezone.w)))
+      ai = "Latest Order Script Status @ " + ah
+      aj = 'Order Script for Photos was ran!'
+      ak = ['version:1', 'application:python']
+      am.api.Event.create(title=ai, text=aj, tags=ak)
+      al.exit(0)
