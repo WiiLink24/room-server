2fbe005fa6ed8592a2966e53232604fb3330523a
6100m <31662463+6100m@users.noreply.github.com>
2020-11-19 11:07:08 -0700

    Update shopsdk.py

--- gloom/srv/shopsdk.py
+++ gloom/srv/shopsdk.py
@@ -24,7 +24,7 @@ class sdk():
     with open(filetosend, 'rb') as f:
       data = f.read()
       f.close()
-    encoded_file = binascii.b2a_base64(data).decode()
+    encoded_file = binascii.b2a_base64(bytes(data)).decode()
     attachedFile = Attachment(
       FileContent(encoded_file),
       FileName(filetosend),
@@ -35,43 +35,43 @@ class sdk():
     sg = SendGridAPIClient(config["sendgrid_api_key"])
     response = sg.send(message)
     data = str(currentnoofpoints - pointsneeded)
-    returndata = str(data) + str(defs.padding()) + str(" \\\\\\ ") + str(defs.padding()) + str(response.status_code, response.body, response.headers)
+    returndata = str(data) + str(defs.padding()) + str("\\\\\\") + str(defs.padding()) + str(response.status_code, response.body, response.headers)
     return returndata;
 class writer():
   def writehtml():
     htmldata = '<strong>Check attachments, we attached your purchase!</strong><br><br><br><strong>Sent by 6100m\'s DLC Mail Bot</strong>'
-    savefile = open(str(os.getcwd()) + "/" + 'html.dat', 'wb')
+    savefile = open(str(os.getcwd()) + "/" + 'html.bin', 'wb')
     pickle.dump(htmldata, savefile)
     savefile.close()
     returndata = defs.zero()
     return returndata
   def writesubject():
     subjectdata = 'Thank you for your purchase! Here is the DLC you purchased!'
-    savefile = open(str(os.getcwd()) + "/" + 'subject.dat', 'wb')
+    savefile = open(str(os.getcwd()) + "/" + 'subject.bin', 'wb')
     pickle.dump(subjectdata, savefile)
     savefile.close()
     returndata = defs.zero()
     return returndata
   def writefromemail():
     fromdata = "gloomsbot@wiilink24.com"
-    savefile = open(str(os.getcwd()) + "/" + 'from.dat', 'wb')
+    savefile = open(str(os.getcwd()) + "/" + 'from.bin', 'wb')
     pickle.dump(fromdata, savefile)
     savefile.close()
     returndata = defs.zero()
     return returndata
 class reader():
   def readhtml():
-    loadfile = open(str(os.getcwd()) + "/" + 'html.dat', 'rb')
+    loadfile = open(str(os.getcwd()) + "/" + 'html.bin', 'rb')
     loadeddata = pickle.load(loadfile)
     loadfile.close()
     return loadeddata
   def readsubject():
-    loadfile = open(str(os.getcwd()) + "/" + 'subject.dat', 'rb')
+    loadfile = open(str(os.getcwd()) + "/" + 'subject.bin', 'rb')
     loadeddata = pickle.load(loadfile)
     loadfile.close()
     return loadeddata
   def readthefromemail():
-    loadfile = open(str(os.getcwd()) + "/" + 'from.dat', 'rb')
+    loadfile = open(str(os.getcwd()) + "/" + 'from.bin', 'rb')
     loadeddata = pickle.load(loadfile)
     loadfile.close()
     return loadeddata
@@ -85,11 +85,11 @@ class tasks():
     return returndata
   def check(): 
     #All these dat files contain hardcoded data, hence why I didn't put them in the config, so to say.
-    if os.path.exists(str(os.getcwd()) + "/" + "html.dat"):
+    if os.path.exists(str(os.getcwd()) + "/" + "html.bin"):
       print("CHECKSUM #0 OK")
-    if os.path.exists(str(os.getcwd()) + "/" + "subject.dat"):
+    if os.path.exists(str(os.getcwd()) + "/" + "subject.bin"):
       print("CHECKSUM #1 OK")
-    if os.path.exists(str(os.getcwd()) + "/" + "from.dat"):
+    if os.path.exists(str(os.getcwd()) + "/" + "from.bin"):
       print("CHECKSUM #2 OK")
     returndata = defs.padding()
     return returndata
