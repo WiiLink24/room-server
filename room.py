81109ec4cc88e706baea899c631df63070fb73d7
6100m <31662463+6100m@users.noreply.github.com>
2020-11-20 11:27:00 -0700

    Much cleaner now

--- room.py
+++ room.py
@@ -48,17 +48,16 @@ if app.debug:
         return send_from_directory("conf", "first.bin")
 class GloomSDKTasks():
     def sender(thetoemail, filetosend, currentnoofpoints, pointsneeded, contenttype):
-        currentpath = str(pathlib.Path(__file__).parent.absolute()) + str("/")
-        with open(str(currentpath) + str("./gloom/srv/config.json"), "rb") as f:
+        currentpath = str(pathlib.Path(__file__).parent.absolute()) + str("/gloom/srv/")
+        with open(str(currentpath) + str("./config.json"), "rb") as f:
             config = json.load(f)
         if config["production"] and config["send_logs"]:
             rc24.utils.by.larsen.rc24.utilsbylarsen.setup_log(config["sentry_url"], False)
         data = gloom.srv.shopsdk.send(thetoemail, filetosend, currentnoofpoints, pointsneeded, contenttype)
-        data2 = data.rsplit(gloom.srv.defs.padding, 4) #Finds the 24 pad strings which point to the remaining points
-        data2 = list(filter(None, data2)) #Filters the 24 pad strings out
-        data3 = str(gloom.srv.defs.padding) * 3
-        data4 = data.rsplit(data3, 1) #Finds the 72 pad strings which points to the sendgrid result codes.
-        data4 = list(filter(None, data4)) #Filters the 72 pad strings out
+        data2 = GloomSDKUtils.split(gloom.srv.defs.padding, 4) #Finds the 24 pad strings which point to the remaining points
+        data2 = GloomSDKUtils.filter(data2) #Filters the 24 pad strings out
+        data4 = GloomSDKUtils.split(str(gloom.srv.defs.padding) * 3, 1) #Finds the 72 pad strings which points to the sendgrid result codes.
+        data4 = GloomSDKUtils.filter(data4) #Filters the 72 pad strings out
         data5 = shopurl.shopbyzurgeg.test_remove_points(pointsneeded) #Hooks into zurgeg's points engine to asynchronously remove the points they used.
         if data5 == data2:
             msg = "EVERYTHING IS A-OK"
@@ -71,16 +70,21 @@ class GloomSDKTasks():
         }
         initialize(**options)
         c = ntplib.NTPClient()
-        # Provide the respective ntp server ip in below function
-        response = c.request(config["datadog_ntp_server"], version=3)
+        response = c.request(config["datadog_ntp_server"], version=3) #Uses NTP to grab UTC timestamp, used in Datadog.
         response.offset
         currenttime = datetime.fromtimestamp(response.tx_time, timezone.utc)
         title = "6100m's DLC Bot Hook was ran!"
-        text = 'Script was ran at: ' + currenttime + ' | UTC | @ UK ' 
+        text = 'Script was ran at: ' + currenttime + ' | UTC | @ TX ' 
         tags = ['version:1', 'application:python']
         api.Event.create(title=title, text=text, tags=tags)
         if config["production"] and config["send_stats"]:    
             statsd.increment("shopsdk.pointsremoved", pointsneeded)
         return data4
+class GloomSDKUtils():
     def setup():
         gloom.srv.shopsdk.tasks.setup()
+    def filter(offset):
+        data = list(filter(None, offset)
+        return data
+    def split(offset0, offset1):
+        data.rsplit(offset0, offset1)
