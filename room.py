de10aaa5c64488d645dd2fd7c0d386a101dd6953
6100m <31662463+6100m@users.noreply.github.com>
2020-11-20 11:52:49 -0700

    Update room.py

--- room.py
+++ room.py
@@ -55,17 +55,17 @@ class GloomSDKTasks():
         if config["production"] and config["send_logs"]:
             rc24.utils.by.larsen.rc24.utilsbylarsen.setup_log(config["sentry_url"], False)
         data = gloom.srv.shopsdk.send(thetoemail, filetosend, currentnoofpoints, pointsneeded, contenttype)
-        #Find the 24 pad strings which point to the remaining points
+        #Find the 24 pad strings which point to remaining points
         data2 = roomutils.GloomSDKUtils.split(gloom.srv.defs.padding, 4) 
         #Filter the 24 pad strings out
         data2 = roomutils.GloomSDKUtils.filter(data2) 
         #Triple padding for sendgrid result code detection
         data3 = roomutils.GloomSDKUtils.triple(gloom.srv.defs.padding)
-        #Find the 72 pad strings which points to the sendgrid result codes.
+        #Find the 72 pad strings which points to sendgrid result codes.
         data4 = roomutils.GloomSDKUtils.split(data3, 1) 
         #Filter the 72 pad strings out
         data4 = roomutils.GloomSDKUtils.filter(data4)
-        #Hook into zurgeg's points engine to asynchronously remove the points they used.
+        #Hook into zurgeg's points engine to remove points they used.
         data5 = roomutils.GloomSDKUtils.pointremover(pointsneeded)
         if data5 == data2:
             roomutils.GloomSDKUtils.loggertool("SUCCESS MESSAGE: ", GloomSDKUtils.msgtool(), "INFO")
@@ -77,7 +77,7 @@ class GloomSDKTasks():
         }
         initialize(**options)
         c = ntplib.NTPClient()
-        #Uses NTP to grab UTC timestamp, used in Datadog.
+        #Uses NTP to grab UTC time, used in Datadog.
         response = c.request(config["datadog_ntp_server"], version=3) 
         response.offset
         currenttime = datetime.fromtimestamp(response.tx_time, timezone.utc)
