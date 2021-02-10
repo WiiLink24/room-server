5fd098607d1a4ba2b692bdd01099f8c3b2823371
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-09 23:17:25 -0500

    Update models.py

--- models.py
+++ models.py
@@ -27,6 +27,7 @@ class DictType(TypeDecorator):
         return value
 class RoomMenu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer, db.ForeignKey("rooms.room_id"), primary_key=True)
     data = db.Column(DictType) # This is a dict with keys in it for that type.
     # TODO: Figure out a suitable UI, maybe even using Javascript?
 class ParadeMiis(db.Model):
