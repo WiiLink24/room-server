1d379abc8cf097d7347996048b06c640344631c7
6100m <77129415+6100m@users.noreply.github.com>
2021-02-16 13:22:13 -0700

    Update reginfo.py

--- url2/reginfo.py
+++ url2/reginfo.py
@@ -1,20 +1,17 @@
 from room import app
 from helpers import xml_node_name, current_date_and_time
-
-
+from ... import timezone_module
+response_data = timezone_module.gettime(request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 @app.route("/url2/reginfo.cgi")
 @xml_node_name("RegionInfo")
 def reginfo_cgi():
     return {
-        "sdt": current_date_and_time(),
-        "cdt": current_date_and_time(),
+        "sdt": str(response_data),
+        "cdt": str(response_data),
         "limited": "0",
     }
-
-
 @app.route("/url1/conf/datetime.xml")
 @xml_node_name("DateTime")
 def datetime_xml():
     # DateTime is roughly equivalent to RegInfo as seen in v1025.
-
-    return {"upddt": current_date_and_time()}
+    return {"upddt": str(response_data)}
