f96fcab56709d4c53a28f293da24fe0c4624ba55
6100m <77129415+6100m@users.noreply.github.com>
2021-02-16 13:39:48 -0700

    Update reginfo.py

--- url2/reginfo.py
+++ url2/reginfo.py
@@ -1,7 +1,14 @@
 from room import app
+from datetime import datetime 
+from pytz import timezone
 from helpers import xml_node_name, current_date_and_time
-from ... import timezone_module
-response_data = timezone_module.gettime(request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+from urllib import urlopen
+from config import ipinfo_token
+from json import load
+from flask import request
+requested_data = str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+json_data = json.load(urlopen('http://ipinfo.io/{}/json?token='.format(requested_data) + ipinfo_token))
+response_data = datetime.now(pytz.timezone(str(json_data['timezone']))).strftime("%Y-%m-%dT%H:%M:%S")) 
 @app.route("/url2/reginfo.cgi")
 @xml_node_name("RegionInfo")
 def reginfo_cgi():
