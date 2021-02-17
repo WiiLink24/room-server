de46306dd90d45fddfc472ee4841fec12a3d9017
6100m <77129415+6100m@users.noreply.github.com>
2021-02-17 13:30:22 -0700

    Finalized

--- url2/reginfo.py
+++ url2/reginfo.py
@@ -1,24 +1,37 @@
-from room import app
-from datetime import datetime 
 from pytz import timezone
-from helpers import xml_node_name, current_date_and_time
-from urllib import urlopen
-from config import ipinfo_token
+from helpers import xml_node_name
+from urllib3 import urlopen
 from json import load
-from flask import request
-requested_data = str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-json_data = json.load(urlopen('http://ipinfo.io/{}/json?token='.format(requested_data) + ipinfo_token))
-response_data = datetime.now(pytz.timezone(str(json_data['timezone']))).strftime("%Y-%m-%dT%H:%M:%S")) 
-@app.route("/url2/reginfo.cgi")
+from config.reginfo_data import ipinfo_token
+from room.app import route
+from datetime.datetime import now
+from flask.request.environ import get
+from flask.request import remote_addr
+
+
+def getzone():
+    requested_data = get("HTTP_X_REAL_IP", remote_addr)
+    json_data = load(
+        urlopen(
+            str("http://ipinfo.io/%s/json?token=%s") % requested_data,
+            ipinfo_token,
+        )
+    )
+    return now(timezone(json_data["timezone"])).strftime("%Y-%m-%dT%H:%M:%S")
+
+
+@route("/url2/reginfo.cgi")
 @xml_node_name("RegionInfo")
 def reginfo_cgi():
     return {
-        "sdt": str(response_data),
-        "cdt": str(response_data),
+        "sdt": getzone(),
+        "cdt": getzone(),
         "limited": "0",
     }
-@app.route("/url1/conf/datetime.xml")
+
+
+@route("/url1/conf/datetime.xml")
 @xml_node_name("DateTime")
 def datetime_xml():
     # DateTime is roughly equivalent to RegInfo as seen in v1025.
-    return {"upddt": str(response_data)}
+    return {"upddt": getzone()}
