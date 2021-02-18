860652a7abe95b9fb1b7fa28755631af3c639e78
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-18 13:46:03 -0500

    Will sort the categories into genres properly
    This lets us sort the categories into specific genres, instead of them appearing in every single category.

--- url3/category.py
+++ url3/category.py
@@ -1,26 +1,56 @@
-from room import app
+from room import app, db
 from helpers import xml_node_name, RepeatedElement
 from models import PayCategories
 
+from werkzeug import exceptions
 
-@app.route("/url3/pay/list/category/<list_id>.xml")
+
+@app.route("/url3/pay/list/category/<int:list_id>.xml")
 @xml_node_name("PayCategoryList")
-def pay_list_category(list_id):
+def pay_list_category(list_id: int):
     queried_categories = PayCategories.query.order_by(PayCategories.name.asc()).all()
+
+    retrieved_data = (
+        db.session.query(PayCategories)
+        .filter(PayCategories.genre_id == list_id)
+        .order_by(PayCategories.category_id)
+        .all()
+    )
     filler = []
-    for i, pay_categories in enumerate(queried_categories):
-        # Items must be indexed by 1.
-        filler.append(
-            RepeatedElement(
-                {
-                    "place": i + 1,
-                    "categid": pay_categories.category_id,
-                    "name": pay_categories.name,
-                    "sppageid": 0,
-                    "splinktext": "Link Text",
-                }
+
+    if list_id <= 9:
+        for i, pay_categories in enumerate(queried_categories):
+            # Items must be indexed by 1.
+            filler.append(
+                RepeatedElement(
+                    {
+                        "place": i + 1,
+                        "categid": pay_categories.category_id,
+                        "name": pay_categories.name,
+                        "sppageid": 0,
+                        "splinktext": "Link Text",
+                    }
+                )
             )
-        )
+    else:
+        if not retrieved_data:
+            # Looks like this category does not exist, or contains no movies.
+            return exceptions.NotFound()
+
+        for i, pay_categories in enumerate(retrieved_data):
+            # This section allows us to sort the categories into genres
+            # Instead of them showing up in all of the genres
+            filler.append(
+                RepeatedElement(
+                    {
+                        "place": i + 1,
+                        "categid": pay_categories.category_id,
+                        "name": pay_categories.name,
+                        "sppageid": 0,
+                        "splinktext": "Link Text",
+                    }
+                )
+            )
 
     return {
         "type": 3,
