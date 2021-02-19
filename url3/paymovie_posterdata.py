bef3ad542f387ba46ca698e549b4483274681fcf
Spotlight <spotlight@joscomputing.space>
2021-02-19 01:51:24 -0600

    Format with black

--- url3/paymovie_posterdata.py
+++ url3/paymovie_posterdata.py
@@ -62,7 +62,7 @@ if app.debug:
         return send_from_directory(
             safe_join("assets/pay-movie/", unk, name), name1 + ".img"
         )
-    
+
     @app.route("/url3/pay/movie/<unk>/<name>/<name1>.smo")
     # Grabs the trailer
     def serve_pay_trailer(unk, name, name1):
