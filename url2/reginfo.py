7594701d4b2adf4f8f9fc56562f456b89d6e51e6
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-02-19 11:03:43 -0500

    Use user timezone

--- url2/reginfo.py
+++ url2/reginfo.py
@@ -1,13 +1,39 @@
 from room import app
+from flask import jsonify, request
 from helpers import xml_node_name, current_date_and_time
+import config
+from geoip2.webservice import Client
+from pytz import timezone
+from datetime import datetime
+
+
+class DummyMMResponse:
+    def __init__(self, **kwargs):
+        self.time_zone = "Etc/UTC"
+
+
+class DummyMMClient:
+    def __init__(*args, **kwargs):
+        # Fake Maxmind client to always return UTC time
+        pass
+
+    def insights(*args, **kwargs):
+        return DummyMMResponse()
+
+
+if config.use_localized_time:
+    client = Client(config.maxmind_account_id, config.maxmind_license_id)
+else:
+    client = DummyMMClient()
 
 
 @app.route("/url2/reginfo.cgi")
 @xml_node_name("RegionInfo")
 def reginfo_cgi():
+    timezone_data = client.city(request.remote_addr).time_zone
     return {
         "sdt": current_date_and_time(),
-        "cdt": current_date_and_time(),
+        "cdt": datetime.now(timezone(timezone_data)).strftime("%Y-%m-%dT%H:%M:%S"),
         "limited": "0",
     }
 
