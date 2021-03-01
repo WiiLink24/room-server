3972880d5d05899d3e7bff5aa693690e336955d0
zurgeg <zurgeg@gmail.com>
2021-03-01 17:11:48 -0500

    Add link form

--- theunderground/forms.py
+++ theunderground/forms.py
@@ -21,6 +21,12 @@ Reference:
         title = form.title.data
 """
 
+class RoomLinkForm(FlaskForm):
+    place = StringField("Place in Room")
+    imageid = StringField("Image ID")
+    title = StringField("Link Title")
+    url = StringField("Link")
+    submit = SubmitField("Done!")
 
 class RoomMovieForm(FlaskForm):
     movie_id = StringField("Movie ID")
