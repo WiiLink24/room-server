234d73dbd5568693f39b0ac3f20a48d46d8cab7e
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-04-20 21:21:26 -0400

    Add encoding for link

--- theunderground/room_paths.py
+++ theunderground/room_paths.py
@@ -50,9 +50,38 @@ def save_vote_data(image_data: bytes, image2_data: bytes, image3_data: bytes, tv
     image3.write(image3_data)
     image3.close()
 
-    
+
 def save_mov_data(pic_num: int, tv_data: bytes):
     tv_data = room_tv_encode(tv_data)
     tv = open(f"assets/special-12/c{pic_num}.img", "wb")
+    tv.write(tv_data)
+    tv.close()
+
+
+def save_link_data(
+    movie_id: int, movie_data: bytes, image1_data: bytes, image2_data: bytes, tv_data: bytes, pic_num: int
+):
+    movie_dir = "assets/urllink"
+
+    # Write movie
+    movie = open(f"{movie_dir}/{movie_id}-H.mov", "wb")
+    movie.write(movie_data)
+    movie.close()
+
+    # Resize and write thumbnail
+    image1_data = room_big_img_encode(image1_data)
+    image1 = open(f"{movie_dir}/{movie_id}-1.img", "wb")
+    image1.write(image1_data)
+    image1.close()
+
+    # Resize and write thumbnail
+    image2_data = room_big_img_encode(image2_data)
+    image2 = open(f"{movie_dir}/{movie_id}.img", "wb")
+    image2.write(image2_data)
+    image2.close()
+
+    # Resize and write poster
+    tv_data = room_tv_encode(tv_data)
+    tv = open(f"assets/special-12/h{pic_num}.img", "wb")
     tv.write(tv_data)
     tv.close()
