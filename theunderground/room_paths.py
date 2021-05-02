80553d8bf00d35debda1488a0c2190861b17f083
Spotlight <spotlight@joscomputing.space>
2021-05-02 15:05:44 -0500

    Simplify room path writing

--- theunderground/room_paths.py
+++ theunderground/room_paths.py
@@ -5,27 +5,26 @@ from theunderground.encodemii import (
 )
 
 
+def write_to_path(filename: str, data: bytes):
+    with open(filename, "wb") as file:
+        file.write(data)
+
+
 def save_delivery_data(
     movie_id: int, movie_data: bytes, image_data: bytes, tv_data: bytes, pic_num: int
 ):
     movie_dir = "assets/delivery"
 
     # Write movie
-    movie = open(f"{movie_dir}/{movie_id}-H.mov", "wb")
-    movie.write(movie_data)
-    movie.close()
+    write_to_path(f"{movie_dir}/{movie_id}-H.mov", movie_data)
 
     # Resize and write thumbnail
     image_data = room_big_img_encode(image_data)
-    image = open(f"{movie_dir}/{movie_id}.img", "wb")
-    image.write(image_data)
-    image.close()
+    write_to_path(f"{movie_dir}/{movie_id}.img", image_data)
 
     # Resize and write poster
     tv_data = room_tv_encode(tv_data)
-    tv = open(f"assets/special-12/a{pic_num}.img", "wb")
-    tv.write(tv_data)
-    tv.close()
+    write_to_path(f"assets/special-12/a{pic_num}.img", tv_data)
 
 
 def save_vote_data(
@@ -38,34 +37,24 @@ def save_vote_data(
 
     # Resize and write poster
     tv_data = room_tv_encode(tv_data)
-    tv = open(f"assets/special-12/b{pic_num}.img", "wb")
-    tv.write(tv_data)
-    tv.close()
+    write_to_path(f"assets/special-12/b{pic_num}.img", tv_data)
 
     # Resize and write poster
     image_data = vote_picture_encode(image_data)
-    image1 = open(f"assets/special-12/e{pic_num}-1.img", "wb")
-    image1.write(image_data)
-    image1.close()
+    write_to_path(f"assets/special-12/e{pic_num}-1.img", image_data)
 
     # Resize and write poster
     image2_data = vote_picture_encode(image2_data)
-    image2 = open(f"assets/special-12/e{pic_num}-2.img", "wb")
-    image2.write(image2_data)
-    image2.close()
+    write_to_path(f"assets/special-12/e{pic_num}-2.img", image2_data)
 
     # Resize and write poster
     image3_data = vote_picture_encode(image3_data)
-    image3 = open(f"assets/special-12/e{pic_num}-3.img", "wb")
-    image3.write(image3_data)
-    image3.close()
+    write_to_path(f"assets/special-12/e{pic_num}-3.img", image3_data)
 
 
 def save_mov_data(pic_num: int, tv_data: bytes):
     tv_data = room_tv_encode(tv_data)
-    tv = open(f"assets/special-12/c{pic_num}.img", "wb")
-    tv.write(tv_data)
-    tv.close()
+    write_to_path(f"assets/special-12/c{pic_num}-1.img", tv_data)
 
 
 def save_link_data(
@@ -79,24 +68,16 @@ def save_link_data(
     movie_dir = "assets/urllink"
 
     # Write movie
-    movie = open(f"{movie_dir}/{movie_id}-H.mov", "wb")
-    movie.write(movie_data)
-    movie.close()
+    write_to_path(f"{movie_dir}/{movie_id}-H.mov", movie_data)
 
     # Resize and write thumbnail
     image1_data = room_big_img_encode(image1_data)
-    image1 = open(f"{movie_dir}/{movie_id}-1.img", "wb")
-    image1.write(image1_data)
-    image1.close()
+    write_to_path(f"{movie_dir}/{movie_id}-1.img", image1_data)
 
     # Resize and write thumbnail
     image2_data = room_big_img_encode(image2_data)
-    image2 = open(f"{movie_dir}/{movie_id}.img", "wb")
-    image2.write(image2_data)
-    image2.close()
+    write_to_path(f"{movie_dir}/{movie_id}.img", image2_data)
 
     # Resize and write poster
     tv_data = room_tv_encode(tv_data)
-    tv = open(f"assets/special-12/h{pic_num}.img", "wb")
-    tv.write(tv_data)
-    tv.close()
+    write_to_path(f"assets/special-12/h{pic_num}.img", tv_data)
