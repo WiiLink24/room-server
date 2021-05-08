218051b05e989b203a3feec564b9931b925b7674
Spotlight <spotlight@joscomputing.space>
2021-05-08 04:15:05 -0500

    Add v770 hotfix

--- url1/wall_metadata.py
+++ url1/wall_metadata.py
@@ -2,13 +2,22 @@ from flask import send_from_directory
 from werkzeug import exceptions
 
 from room import app
-from helpers import xml_node_name
+from helpers import xml_node_name, is_v770
 from models import Posters
 
 
 @app.route("/url1/wall/<int:met_id>.met")
 @xml_node_name("PosterMeta")
 def wall_metadata(met_id: int):
+    # v770 requires we have poster metadata for the poster in the back of the room.
+    if met_id == 2 and is_v770:
+        return {
+            "posterid": 2,
+            "msg": "Welcome to Wii Room!",
+            "movieid": 1,
+            "title": "Hello, v770!",
+        }
+
     # Determine if we have metadata for this poster.
     poster_metadata = Posters.query.filter_by(poster_id=met_id).first()
     if poster_metadata is None:
