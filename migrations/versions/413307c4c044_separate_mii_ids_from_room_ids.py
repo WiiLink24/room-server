129a3ac31447c8044e375d30118f402655b2c34b
Spotlight <spotlight@joscomputing.space>
2021-09-28 20:38:02 -0500

    Migrate disk assets as well

--- migrations/versions/413307c4c044_separate_mii_ids_from_room_ids.py
+++ migrations/versions/413307c4c044_separate_mii_ids_from_room_ids.py
@@ -5,6 +5,8 @@ Revises: d47aeb04a99c
 Create Date: 2021-09-28 15:44:20.490928
 
 """
+import os
+
 from alembic import op
 import sqlalchemy as sa
 
@@ -14,6 +16,20 @@ revision = "413307c4c044"
 down_revision = "d47aeb04a99c"
 branch_labels = None
 depends_on = None
+
+
+def get_tables() -> (sa.table, sa.table):
+    rooms = sa.table(
+        "rooms",
+        sa.Column("room_id", sa.Integer),
+        sa.Column("mii_id", sa.Integer),
+    )
+    room_menu = sa.table(
+        "room_menu",
+        sa.Column("room_id", sa.Integer, primary_key=True),
+    )
+
+    return rooms, room_menu
 
 
 def upgrade():
@@ -36,16 +52,16 @@ def upgrade():
     )
     op.create_unique_constraint(None, "rooms", ["room_id"])
 
+    rooms, room_menu = get_tables()
+
+    # Migrate disk assets
+    room_values = op.get_bind().execute("SELECT room_id, mii_id FROM rooms").fetchall()
+    for room_id, mii_id in room_values:
+        old_path = f"./assets/special-{mii_id}"
+        new_path = f"./assets/special/{room_id}"
+        os.renames(old_path, new_path)
+
     # Update room_menu to use room ID over mii ID
-    rooms = sa.table(
-        "rooms",
-        sa.Column("room_id", sa.Integer),
-        sa.Column("mii_id", sa.Integer),
-    )
-    room_menu = sa.table(
-        "room_menu",
-        sa.Column("room_id", sa.Integer, primary_key=True),
-    )
     op.execute(
         room_menu.update()
         .where(room_menu.c.room_id == rooms.c.mii_id)
@@ -58,16 +74,15 @@ def upgrade():
 
 
 def downgrade():
+    # Revert disk asset movement
+    room_values = op.get_bind().execute("SELECT room_id, mii_id FROM rooms").fetchall()
+    for room_id, mii_id in room_values:
+        old_path = f"./assets/special/{room_id}"
+        new_path = f"./assets/special-{mii_id}"
+        os.renames(old_path, new_path)
+
     # Revert room_menu
-    rooms = sa.table(
-        "rooms",
-        sa.Column("room_id", sa.Integer),
-        sa.Column("mii_id", sa.Integer),
-    )
-    room_menu = sa.table(
-        "room_menu",
-        sa.Column("room_id", sa.Integer, primary_key=True),
-    )
+    rooms, room_menu = get_tables()
     op.execute(
         room_menu.update()
         .where(room_menu.c.room_id == rooms.c.room_id)
