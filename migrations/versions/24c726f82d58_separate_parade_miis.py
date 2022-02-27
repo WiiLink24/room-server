30ab30e2f0c5c2ca5bb778e13e305a523fcec79f
Spotlight <spotlight@joscomputing.space>
2022-02-26 19:52:25 -0600

    Transfer news from parade_miis
    This should fully resolve migration woes.

--- migrations/versions/24c726f82d58_separate_parade_miis.py
+++ migrations/versions/24c726f82d58_separate_parade_miis.py
@@ -34,7 +34,8 @@ def upgrade():
         ),
         sa.PrimaryKeyConstraint("id"),
     )
-    # Add a level column for transfer purposes.
+    # Add level and news columns for transfer purposes.
+    op.add_column("rooms", sa.Column("news", sa.Integer(), nullable=True))
     op.add_column("rooms", sa.Column("level", sa.Integer(), nullable=True))
 
     # Before we drop parade_miis and similar, we need to salvage
@@ -59,7 +60,7 @@ def upgrade():
     )
     op.get_bind().execute(
         "UPDATE rooms "
-        "SET level = parade_miis.level, mii_msg = parade_miis.mii_msg "
+        "SET level = parade_miis.level, mii_msg = parade_miis.mii_msg, news = parade_miis.news "
         "FROM parade_miis "
         "WHERE rooms.mii_id = parade_miis.mii_id"
     )
