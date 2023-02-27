52ee9c45e6e0143a34b421e11fc128ee506d6b5f
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2023-02-26 23:38:10 -0500

    Try something else for delivery

--- url2/delivery.py
+++ url2/delivery.py
@@ -1,7 +1,9 @@
 from room import app
+from helpers import xml_node_name
 
 
 @app.route("/url2/smp.cgi", methods=["POST"])
+@xml_node_name("Delivery")
 def handle_delivery():
     """This is 100% not apart of url2 however I don't know where else to put this"""
-    return "OK"
+    return {"code": 1, "msg": "Vote recorded."}
