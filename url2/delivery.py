c0b16298250e6fea3e7a83fafa86634f58dfadab
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2023-02-27 08:29:26 -0500

    Fix overmapping function error

--- url2/delivery.py
+++ url2/delivery.py
@@ -4,6 +4,6 @@ from helpers import xml_node_name
 
 @app.route("/url2/smp.cgi", methods=["POST"])
 @xml_node_name("Delivery")
-def handle_delivery():
+def delivery_response():
     """This is 100% not apart of url2 however I don't know where else to put this"""
     return {"code": 1, "msg": "Vote recorded."}
