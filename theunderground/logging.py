69697000aed949f7a04a8c6c4548e1d731dc6b9b
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2025-02-03 17:01:03 -0500

    feat: Add log interface

--- theunderground/logging.py
+++ theunderground/logging.py
@@ -1,8 +1,9 @@
 from models import Logs, db
 from .admin import oidc
 from room import app
-from flask import session
+from flask import session, render_template, request
 import datetime
+from theunderground.admin import oidc
 
 def log_action(action: str):
     obj = Logs(
@@ -13,3 +14,15 @@ def log_action(action: str):
 
     db.session.add(obj)
     db.session.commit()
+
+
+@app.route("/theunderground/logs")
+@oidc.require_login
+def show_logs():
+    page_num = request.args.get("page", default=1, type=int)
+
+    logs = Logs.query.order_by(Logs.timestamp.desc()).paginate(
+        page=page_num, per_page=15, error_out=False
+    )
+
+    return render_template("list_logs.html", logs=logs, type_length=logs.total)
