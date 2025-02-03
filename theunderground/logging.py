2ca652f60d3a84fe841e2bead0a48c17ae3ca740
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2025-02-03 16:43:16 -0500

    fix: Store current time in database for log

--- theunderground/logging.py
+++ theunderground/logging.py
@@ -2,11 +2,13 @@ from models import Logs, db
 from .admin import oidc
 from room import app
 from flask import session
+import datetime
 
 def log_action(action: str):
     obj = Logs(
         action=action,
-        user=session["oidc_auth_profile"]["nickname"]
+        user=session["oidc_auth_profile"]["nickname"],
+        timestamp= datetime.datetime.now()
     )
 
     db.session.add(obj)
