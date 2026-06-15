f8bece1a6456e290bcaa7175a54e144df46d11b1
Noah <75850871+noahpistilli@users.noreply.github.com>
2026-06-15 05:02:34 -0400

    Bump flask-sqlalchemy + Refactor Models (#88)
    * chore: Bump flask-sqlalchemy
    
    * feat: Refactor models

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 import enum
 
-from flask_sqlalchemy import BaseQuery, SQLAlchemy
-from sqlalchemy import func
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.query import Query
+from sqlalchemy import func, String, ForeignKey, LargeBinary, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import TypeDecorator
 from sqlalchemy_searchable import make_searchable
 from sqlalchemy_utils import TSVectorType
+from datetime import datetime
+from typing import Optional
 
 import sqlalchemy
 import json
@@ -28,33 +32,33 @@ class DictType(TypeDecorator):
 
 
 class RoomMenu(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    room_id = db.Column(db.Integer)
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    room_id: Mapped[int]
     data = db.Column(DictType)  # This is a dict with keys in it for that type.
     # TODO: Figure out a suitable UI, maybe even using Javascript?
 
 
 class Posters(db.Model):
-    poster_id = db.Column(db.Integer, primary_key=True, unique=True)
-    msg = db.Column(db.String(15), nullable=False)
-    movie_id = db.Column(db.Integer, nullable=False)
+    poster_id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    msg: Mapped[str] = mapped_column(String(15))
+    movie_id: Mapped[int]
     # While a maximum of 47 characters seems arbitrary, it is what it is.
     # It appears they mandate the last byte in their char array is null (and sprintf),
     # so perhaps the internal type is char[48]?
-    title = db.Column(db.String(47), nullable=False)
+    title: Mapped[str] = mapped_column(String(47))
 
 
 class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    msg = db.Column(db.String(146), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    msg: Mapped[str] = mapped_column(String(146))
 
 
 class PayPosters(db.Model):
-    poster_id = db.Column(db.Integer, primary_key=True, unique=True)
-    msg = db.Column(db.String(15), nullable=False)
-    title = db.Column(db.String(47), nullable=False)
-    type = db.Column(db.Integer, nullable=False)
-    aspect = db.Column(db.Boolean, nullable=False)
+    poster_id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    msg: Mapped[str] = mapped_column(String(15))
+    title: Mapped[str] = mapped_column(String(47))
+    type: Mapped[int]
+    aspect: Mapped[bool]
 
 
 class ConciergeMiiActions(enum.Enum):
@@ -95,41 +99,39 @@ class ConciergeMiiActions(enum.Enum):
 
 
 class ConciergeMiis(db.Model):
-    mii_id = db.Column(
-        db.Integer, db.ForeignKey("mii_data.mii_id"), primary_key=True, unique=True
+    mii_id: Mapped[int] = mapped_column(
+        ForeignKey("mii_data.mii_id"), primary_key=True, unique=True
     )
-    clothes = db.Column(db.Integer, nullable=False)
-    action = db.Column(db.Enum(ConciergeMiiActions), nullable=False)
-    prof = db.Column(db.String(129), nullable=False)
-    movie_id = db.Column(db.Integer, nullable=False)
-    voice = db.Column(db.Boolean, default=False, nullable=False)
+    clothes: Mapped[int]
+    action: Mapped[ConciergeMiiActions]
+    prof: Mapped[str] = mapped_column(String(129))
+    movie_id: Mapped[int]
+    voice: Mapped[bool] = mapped_column(default=False)
 
 
 # MiiData provides the genuine Mii alongside other common information.
 class MiiData(db.Model):
-    mii_id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.LargeBinary(74), nullable=False)
-    name = db.Column(db.String(10), nullable=False)
-    color1 = db.Column(db.String(6), nullable=False)
-    color2 = db.Column(db.String(6), nullable=False)
+    mii_id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    data: Mapped[LargeBinary] = mapped_column(LargeBinary(74))
+    name: Mapped[str] = mapped_column(String(10))
+    color1: Mapped[str] = mapped_column(String(6))
+    color2: Mapped[str] = mapped_column(String(6))
 
 
 class MiiMsgInfo(db.Model):
     # We make both the Mii's ID and message type unique, as to allow row
     # identification for SQLAlchemy.
-    mii_id = db.Column(
-        db.Integer, db.ForeignKey("mii_data.mii_id"), primary_key=True, nullable=False
-    )
-    type = db.Column(db.Integer, primary_key=True, nullable=False)
-    seq = db.Column(db.Integer, primary_key=True, nullable=False)
-    msg = db.Column(db.String(51), nullable=False)
-    face = db.Column(db.Integer, nullable=False)
+    mii_id: Mapped[int] = mapped_column(ForeignKey("mii_data.mii_id"), primary_key=True)
+    type: Mapped[int] = mapped_column(primary_key=True)
+    seq: Mapped[int] = mapped_column(primary_key=True)
+    msg: Mapped[str] = mapped_column(String(51))
+    face: Mapped[int]
 
 
 make_searchable(db.metadata)
 
 
-class FullTextSearchable(BaseQuery):
+class FullTextSearchable(Query):
     pass
 
 
@@ -158,65 +160,65 @@ class MovieGenres(enum.Enum):
 class Movies(db.Model):
     query_class = FullTextSearchable
 
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    category_id = db.Column(
-        db.Integer,
-        db.ForeignKey("categories.category_id"),
-        primary_key=True,
-        nullable=False,
+    movie_id: Mapped[int] = mapped_column(
+        autoincrement=True, primary_key=True, unique=True
     )
-    title = db.Column(db.String(47), nullable=False)
-    length = db.Column(db.String(8), nullable=False)
-    aspect = db.Column(db.Boolean, nullable=False)
-    genre = db.Column(db.Enum(MovieGenres), nullable=False)
-    sp_page_id = db.Column(db.Integer, nullable=False)
-    ds_mov_id = db.Column(db.Integer, nullable=True)
-    staff = db.Column(db.Boolean, nullable=False)
-    date_added = db.Column(db.DateTime, nullable=False, server_default=func.now())
-    unlisted = db.Column(db.Boolean, nullable=False, default=False)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.category_id"),
+        primary_key=True,
+    )
+    title: Mapped[str] = mapped_column(String(47))
+    length: Mapped[str] = mapped_column(String(8))
+    aspect: Mapped[bool]
+    genre: Mapped[MovieGenres]
+    sp_page_id: Mapped[int]
+    ds_mov_id: Mapped[Optional[int]]
+    staff: Mapped[bool]
+    date_added: Mapped[datetime] = mapped_column(server_default=func.now())
+    unlisted: Mapped[bool] = mapped_column(default=False)
 
-    search_vector = db.Column(TSVectorType("title"))
+    search_vector = mapped_column(TSVectorType("title"))
 
 
 class PayMovies(db.Model):
     query_class = FullTextSearchable
 
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    title = db.Column(db.String(47), nullable=False)
-    length = db.Column(db.String, nullable=False)
-    note = db.Column(db.String(76), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    released = db.Column(db.String(10), nullable=False)
-    category_id = db.Column(
-        db.Integer,
-        db.ForeignKey("pay_categories.category_id"),
-        primary_key=True,
-        nullable=False,
+    movie_id: Mapped[int] = mapped_column(
+        autoincrement=True, primary_key=True, unique=True
     )
-    reference_id = db.Column(db.String(32), nullable=False)
+    title: Mapped[str] = mapped_column(String(47))
+    length: Mapped[str]
+    note: Mapped[str] = mapped_column(String(76))
+    price: Mapped[int]
+    released: Mapped[str] = mapped_column(String(10))
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("pay_categories.category_id"),
+        primary_key=True,
+    )
+    reference_id: Mapped[str] = mapped_column(String(32))
     # Must be 7 digits long
-    price_code = db.Column(db.Integer, autoincrement=True, nullable=False)
-    date_added = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    price_code: Mapped[int] = mapped_column(autoincrement=True)
+    date_added: Mapped[datetime] = mapped_column(server_default=func.now())
     search_vector = db.Column(TSVectorType("title", "note"))
 
 
 class PayCategories(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(61))
+    category_id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    name: Mapped[str] = mapped_column(String(61))
     # Starts at 10, goes up by 1 each time
-    genre_id = db.Column(db.Integer)
-    sp_page_id = db.Column(db.Integer)
+    genre_id: Mapped[Optional[int]]
+    sp_page_id: Mapped[Optional[int]]
 
 
 class PayCategoryHeaders(db.Model):
-    title = db.Column(db.String(30), primary_key=True, unique=True)
+    title: Mapped[str] = mapped_column(String(30), primary_key=True, unique=True)
 
 
 class Categories(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(61))
-    sp_page_id = db.Column(db.Integer)
-    unlisted = db.Column(db.Boolean, nullable=False, default=False)
+    category_id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    name: Mapped[str] = mapped_column(String(61))
+    sp_page_id: Mapped[Optional[int]]
+    unlisted: Mapped[bool] = mapped_column(default=False)
 
 
 class RoomBGMTypes(enum.Enum):
@@ -269,44 +271,46 @@ class RoomContentBGMTypes(enum.Enum):
 
 
 class Rooms(db.Model):
-    room_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    bgm = db.Column(db.Enum(RoomBGMTypes))
-    parade_mii = db.Column(db.Integer, db.ForeignKey("mii_data.mii_id"))
-    mascot = db.Column(db.Boolean)
+    room_id: Mapped[int] = mapped_column(
+        autoincrement=True, primary_key=True, unique=True
+    )
+    bgm: Mapped[RoomBGMTypes]
+    parade_mii: Mapped[int] = mapped_column(ForeignKey("mii_data.mii_id"))
+    mascot: Mapped[bool]
     # Each message has a limit of 51. However, there can be 10 sequences. How we handle sequences is by splitting the
     # text by newline.
-    intro_msg = db.Column(db.String)
-    contact_data = db.Column(db.String(2599))
-    news = db.Column(db.String(41))
-    level = db.Column(db.Integer, default=1)
+    intro_msg: Mapped[str]
+    contact_data: Mapped[str] = mapped_column(String(2599))
+    news: Mapped[str] = mapped_column(String(41))
+    level: Mapped[int] = mapped_column(default=1)
 
 
 class RoomMiis(db.Model):
-    room_id = db.Column(db.Integer, db.ForeignKey("rooms.room_id"), nullable=False)
-    mii_id = db.Column(
-        db.Integer, db.ForeignKey("mii_data.mii_id"), unique=True, primary_key=True
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.room_id"))
+    mii_id: Mapped[int] = mapped_column(
+        ForeignKey("mii_data.mii_id"), unique=True, primary_key=True
     )
     # Same thing as intro_msg in Rooms.
-    mii_msg = db.Column(db.String)
-    seq = db.Column(db.Integer, nullable=False)
+    mii_msg: Mapped[str]
+    seq: Mapped[int]
 
 
 class EvaluateData(db.Model):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    movie_id = db.Column(db.Integer, index=True)
-    gender = db.Column(db.Integer)
-    blood = db.Column(db.Integer)
-    age = db.Column(db.Integer)
-    vote = db.Column(db.Integer)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    movie_id: Mapped[int] = mapped_column(index=True)
+    gender: Mapped[int]
+    blood: Mapped[int]
+    age: Mapped[int]
+    vote: Mapped[int]
 
 
 class PollData(db.Model):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    poll_id = db.Column(db.Integer, index=True)
-    wii_num = db.Column(db.BigInteger)
-    choice = db.Column(db.Integer)
-    age = db.Column(db.Integer)
-    gender = db.Column(db.Integer)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    poll_id: Mapped[int] = mapped_column(index=True)
+    wii_num: Mapped[int] = mapped_column(BigInteger)
+    choice: Mapped[int]
+    age: Mapped[int]
+    gender: Mapped[int]
 
 
 class ContentTypes(enum.Enum):
@@ -346,49 +350,41 @@ class LinkTypes(enum.Enum):
 
 
 class IntroInfo(db.Model):
-    cnt_id = db.Column(
-        db.BigInteger, autoincrement=True, primary_key=True, nullable=False, unique=True
+    cnt_id: Mapped[int] = mapped_column(
+        BigInteger, autoincrement=True, primary_key=True, unique=True
     )
-    cnt_type = db.Column(db.Enum(ContentTypes))
-    cat_name = db.Column(db.String(61))
-    link_type = db.Column(db.Enum(LinkTypes))
-    link_id = db.Column(db.Integer)
-    position = db.Column(db.Integer, nullable=False)
+    cnt_type: Mapped[ContentTypes]
+    cat_name: Mapped[Optional[str]] = mapped_column(String(61))
+    link_type: Mapped[LinkTypes]
+    link_id: Mapped[Optional[int]]
+    position: Mapped[int]
 
 
 class Giveaways(db.Model):
-    id = db.Column(
-        db.Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
-    giveaway_id = db.Column(db.Integer, nullable=False)
-    wii_id = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    giveaway_id: Mapped[int]
+    wii_id: Mapped[str]
+    email: Mapped[str]
 
 
 class MovieCredits(db.Model):
-    id = db.Column(
-        db.Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
-    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
-    role = db.Column(db.String(30), nullable=False)
-    name = db.Column(db.String(30), nullable=False)
-    order = db.Column(db.Integer, nullable=False)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.movie_id"))
+    role: Mapped[str] = mapped_column(String(30), nullable=False)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    order: Mapped[int]
 
 
 class ConciergeMovies(db.Model):
     """Movies that are linked to a concierge."""
 
-    id = db.Column(
-        db.Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
-    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"), nullable=False)
-    mii_id = db.Column(db.Integer, db.ForeignKey("mii_data.mii_id"), nullable=False)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.movie_id"), nullable=False)
+    mii_id: Mapped[int] = mapped_column(ForeignKey("mii_data.mii_id"), nullable=False)
 
 
 class Logs(db.Model):
-    id = db.Column(
-        db.Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
-    action = db.Column(db.String, nullable=False)
-    user = db.Column(db.String, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    action: Mapped[str]
+    user: Mapped[str]
+    timestamp: Mapped[datetime]
