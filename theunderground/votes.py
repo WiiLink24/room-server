f1e4a7a493350a94879963344da620b9811993cf
nami1 <135884307+nami1yt@users.noreply.github.com>
2026-07-04 04:40:46 +0000

    fix: Sorting localization Votes

--- theunderground/votes.py
+++ theunderground/votes.py
@@ -12,18 +12,22 @@ from flask import (
 )
 
 from asset_data import NormalCategoryAsset
-from models import Categories, Movies, EvaluateData
+from models import Categories, Movies, EvaluateData, db
 from room import app, s3
 from theunderground.admin import oidc
 from theunderground.mobiclip import get_movie_path
+from theunderground.locale import get_current_locale
 
 
 @app.route("/theunderground/votes")
 def votes_list_categories():
     page_num = request.args.get("page", default=1, type=int)
 
-    categories = Categories.query.order_by(Categories.category_id.asc()).paginate(
-        page=page_num, per_page=15, error_out=False
+    categories = (
+        db.session.query(Categories)
+        .where(Categories.locale == get_current_locale())
+        .order_by(Categories.category_id.asc())
+        .paginate(page=page_num, per_page=15, error_out=False)
     )
 
     votes = {}
